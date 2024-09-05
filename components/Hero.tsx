2c60e444c9eaba7d2c1f22437f2561b6db7bb310
iuliadombrovschi <iulia.dombrovschi@isa.utm.md>
2024-09-05 15:00:07 +0300

    Hero Section
    05.09.24

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -17,12 +17,21 @@ const Hero = () => {
                  with our effortless booking process.
                 </p>
                 <CustomButton
-                title="Explore Cars"
-                containerStyles="bg-primary-blue
-                text-white rounded-full mt-10"
-                handleClick={handleScroll}
-                 />
+  title="Explore Cars"
+  containerStyles="bg-primary-blue text-white rounded-full mt-10"
+  handleClick={handleScroll}
+/>
+
                 
+            </div>
+            <div className="hero__image-container">
+             <div className="hero__image">
+                <Image src="/hero.png" alt="hero"
+                fill className="object-contain"/>
+                <div className="hero__image-overlay">
+                    
+                </div>
+             </div>
             </div>
         </div>
     )
--- types/index.ts
+++ types/index.ts
@@ -3,6 +3,6 @@ import { MouseEventHandler } from "react";
 export interface CustomButtonProps{
     title: string;
     containerStyles?: string;
-    handleClick:
+    handleClick?:
     MouseEventHandler<HTMLButtonElement>;
 }
