80d9bcb319e156f91b89b2cc9a94bcdddc339be9
iuliadombrovschi <iulia.dombrovschi@isa.utm.md>
2024-09-10 19:20:37 +0300

    car iamges

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -4,7 +4,7 @@ import {useState} from 'react'
 import Image from 'next/image';
 import { CarsProps } from '@/types';
 import CustomButton from './CustomButton';
-import { calculateCarRent } from '@/utils';
+import { calculateCarRent, generateCarImageUrl } from '@/utils';
 import CarDetails from './CarDetails';
 
 interface CarCardProps{
@@ -40,7 +40,7 @@ const CarCard = ({car}: CarCardProps) => {
 
 </p>
 <div className="relative w-full h-40 my-3 object-contain">
-<Image src="/hero.png" alt="car model" fill priority
+<Image src={generateCarImageUrl(car)} alt="car model" fill priority
 className="object-contain"/>
 </div>
 
--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,4 @@
+import { CarsProps } from "@/types";
 
 export async function fetchCars(){
     const headers ={
@@ -5,7 +6,7 @@ export async function fetchCars(){
 		'X-RapidAPI-host': 'cars-by-api-ninjas.p.rapidapi.com'
     }
 
-    const Response = await fetch ('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera',
+    const Response = await fetch ('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3',
         {
             headers: headers,
         
@@ -29,3 +30,16 @@ export const calculateCarRent = (city_mpg:number,year:number) => {
 
     return rentalRatePerDay.toFixed(0);
 };
+ export const generateCarImageUrl =(car:CarsProps,angle?: string) => {
+    const url = new URL ('https://cdn.imagin.studio/getimage');
+    const {make,year,model} = car;
+
+    url.searchParams.append('customer','hrjavascript-mastery');
+    url.searchParams.append('make',make);
+    url.searchParams.append('modelFamily',model.split(' ')[0]);
+    url.searchParams.append('zoomType','fullscreen');
+    url.searchParams.append('modelYear',`${year}`);
+    url.searchParams.append('angle',`${angle}`);
+    
+    return `${url}`;
+ }
